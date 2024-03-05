7e513609bcc524a198c01c024537ea2c5de1f9c6
Snehashis Dasgupta <snehashis.jisu.cse@gmail.com>
2024-03-05 13:44:08 +0500

    Completed the UI design of home page[includes- Search, SortRepos, ProfileInfo, Repos]

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,18 @@
+import ProfileInfo from "../components/ProfileInfo"
+import Repos from "../components/Repos"
+import Search from "../components/Search"
+import SortRepos from "../components/SortRepos"
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className="m-4">
+      <Search />
+      <SortRepos />
+      <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
+        <ProfileInfo />
+        <Repos />
+      </div>
+    </div>
   )
 }
 
